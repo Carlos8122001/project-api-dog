c4cf1c8e026c707801ca4a61cf0cdeb5e93be66d
Carlos <karlos8122001@gmail.com>
2023-06-19 21:31:59 -0400

    añadiendo los archivos

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { Box, CardFooter, Divider, Heading, Stack } from "@chakra-ui/react";
+import DogCard from "./components/DogCard";
+
+const dogAPI = "https://dog.ceo/api/breeds/image/random/height=300px";
+
+function App() {
+  return (
+    <>
+      <Box
+        display={"flex"}
+        justifyContent={"center"}
+        alignItems={"center"}
+        width={"100%"}
+        height={"100vh"}
+      >
+        <DogCard />
+      </Box>
+      <Heading size="sm" textAlign={"center"} margin={"15px"} opacity={"0.7"}>© 2023 – Creation of Carlos Velásquez by Dog API</Heading>
+    </>
+  );
+}
+
+export default App;
